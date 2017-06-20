4a93a9b0d8e555f901fd21513bb10f9983335501
moznion <moznion@gmail.com>
2017-06-21 01:13:20 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+List::Haystack - It's new $module
+
+# SYNOPSIS
+
+    use List::Haystack;
+
+# DESCRIPTION
+
+List::Haystack is ...
+
+# LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+moznion <moznion@gmail.com>
---
+++ lib/List/Haystack.pm
@@ -0,0 +1,39 @@
+package List::Haystack;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+List::Haystack - It's new $module
+
+=head1 SYNOPSIS
+
+    use List::Haystack;
+
+=head1 DESCRIPTION
+
+List::Haystack is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
